375ebde23a0157d11c26d72dbced6eb731e4b86a
Rahim sikder <arsikder323@gmail.com>
2023-11-04 15:43:41 +0600

    odd even done

---
+++ even-odd.js
@@ -0,0 +1,11 @@
+function evenOdd (num){
+   const remainder = num % 2;
+   if (remainder == 0){
+      console.log ('number is even')
+   }
+   else{
+      console.log('number is odd')
+   }
+  
+}
+evenOdd(45);
---
+++ var-let-const.js
@@ -0,0 +1,26 @@
+// convert inch to feat
+// const dataInches = 30;
+// const dadaFeat = dataInches/12;
+// console.log(dadaFeat);
+
+// function inchToFeet(inches){
+//      const feet = inches/12;
+//      return feet;
+// }
+// const dadaInches = 144;
+// const dadaFeet = inchToFeet(dadaInches)
+// console.log('dada feet',dadaFeet);
+
+// const nanaInches =168;
+// const nanaFeet = inchToFeet(dadaInches);
+// console.log('nana feet',nanaFeet);
+
+// mile to km convert
+function mileToKm (mile){
+   const km = mile * 1.60934;
+   return km;
+
+}
+const inputMile = 2;
+const totalKm = mileToKm (inputMile);
+console.log(totalKm);
