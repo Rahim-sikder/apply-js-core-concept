9ef1c1161133a0b823185f9b4ec5e97604005165
Rahim sikder <arsikder323@gmail.com>
2023-11-07 02:35:56 +0600

    convert hour to minit

---
+++ minitToHour.js
@@ -0,0 +1,7 @@
+function hourToMinit(hour){
+   let minit = hour * 60;
+   return minit;
+
+}
+let inputHour = hourToMinit (3);
+console.log("after convert hour to minit:",inputHour)
---
+++ whilefactorial.js
@@ -0,0 +1,12 @@
+function factorial(num){
+   let i = num;
+   let result = 1;
+  
+   while(i >= 1){
+       result =result * 1;
+       num -- ;
+   }
+   return result;
+}
+const num = 9;
+const output = factorial (num);
